c98a2ef06818d13d55a6d5252b28bf68941cd581
Ashraful Islam <rx.ju.edu@gmail.com>
2024-07-10 15:02:44 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./styles.css";
+import React, { useEffect } from "react";
+import commentsApi from "./api/comments";
+import postsApi from "./api/posts";
+import useApi from "./hooks/useApi";
+
+export default function App() {
+  const getPostsApi = useApi(postsApi.getPosts);
+  const getCommentsApi = useApi(commentsApi.getComments);
+
+  useEffect(() => {
+    getPostsApi.request();
+    getCommentsApi.request();
+  }, []);
+
+  return (
+    <div className="App">
+      {/* Post List */}
+      <div>
+        <h1>Posts</h1>
+        {getPostsApi.loading && <p>Posts are loading!</p>}
+        {getPostsApi.error && <p>{getPostsApi.error}</p>}
+        <ul>
+          {getPostsApi.data?.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
+      {/* Comment List */}
+      <div>
+        <h1>Comments</h1>
+        {getCommentsApi.loading && <p>Comments are loading!</p>}
+        {getCommentsApi.error && <p>{getCommentsApi.error}</p>}
+        <ul>
+          {getCommentsApi.data?.map((comment) => (
+            <li key={comment.id}>{comment.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
